e1784a3577f6ab99b46419ba413552bea8225913
Hendrik Sollich <hendrik@hoodie.de>
2015-10-25 19:52:27 +0100

    unRefectored Regex (removes valid cases)

--- src/datetime_regex_pure.rs
+++ src/datetime_regex_pure.rs
@@ -20,18 +20,19 @@ impl PureRegexParser {
                 -?(\d{2}) # day
                 $"##).expect("Regex broken"),
             exp: Regex::new(r##"(?x) ^
-                (\d{2})          # hour
-                (?: : (\d{2}))?  # minute
+                (\d{2}) :?     # hour
+                (\d{2})? :?    # minute
 
                 (?:
-                    : (\d{2})           # second
-                    (?: \. (\d{1,9}))?  # millisecond
+                    (\d{2})         # second
+                    \.?
+                    ((?:\d{1,9}))?  # millisecond
                 )?
 
-                (?:                  # time zone offset:
-                    (Z) |            # or just Z for UTC
-                    ([+-]\d\d)?      # hour and
-                    (?: : (\d\d))?   # minute,
+                (?:                 # time zone offset:
+                    (Z) |           # or just Z for UTC
+                    ([+-]\d\d)? :?  # hour and
+                    (\d\d)?         # minute,
                 )?
             $"##).expect("Regex broken"),
         }
