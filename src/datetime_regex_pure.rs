6cdb2a9665f1db7ffab1cfd6e1a47b394c2965ab
Hendrik Sollich <hendrik@hoodie.de>
2021-03-25 19:03:09 +0100

    fmt

--- src/datetime_regex_pure.rs
+++ src/datetime_regex_pure.rs
@@ -10,17 +10,24 @@ pub struct PureRegexParser {
 impl PureRegexParser {
     pub fn new() -> Self {
         PureRegexParser {
-            week: Regex::new(r##"(?x)^
+            week: Regex::new(
+                r##"(?x)^
                 (\d{4})   # year
                 -W(\d{2}) # number of week
                 -(\d{1})  # day in week (1..7)
-                $"##).expect("Regex broken"),
-            ymd: Regex::new(r##"(?x)^
+                $"##,
+            )
+            .expect("Regex broken"),
+            ymd: Regex::new(
+                r##"(?x)^
                 (\d{4})   # year
                 -?(\d{2}) # month
                 -?(\d{2}) # day
-                $"##).expect("Regex broken"),
-            exp: Regex::new(r##"(?x) ^
+                $"##,
+            )
+            .expect("Regex broken"),
+            exp: Regex::new(
+                r##"(?x) ^
         (\d{2})          # hour
         (?: : (\d{2}))?  # minute
 
@@ -34,72 +41,105 @@ impl PureRegexParser {
             ([+-]\d\d)?      # hour and
             (?: : (\d\d))?   # minute,
         )?
-            $"##).expect("Regex broken"),
+            $"##,
+            )
+            .expect("Regex broken"),
         }
     }
 
     /// Splits DateString, TimeString
     ///
     /// for further parsing by `parse_iso_8601_date` and `parse_iso_8601_time`.
-    pub fn split_iso_8601<'a>(&self, string: &'a str) -> (&'a str, &'a str)
-    {
+    pub fn split_iso_8601<'a>(&self, string: &'a str) -> (&'a str, &'a str) {
         if let Some(offset) = string.find('T') {
-            (&string[..offset], &string[offset + 1 ..])
+            (&string[..offset], &string[offset + 1..])
         } else {
             (&string[..], "")
         }
     }
 
     /// Parses a ISO 8601 strin into LocalDateTime Object.
-    pub fn parse_iso_8601(&self, string: &str) -> ((u32,u32,u32),(i8,i8,i8,i32))
-    {
+    pub fn parse_iso_8601(&self, string: &str) -> ((u32, u32, u32), (i8, i8, i8, i32)) {
         let (date_string, time_string) = self.split_iso_8601(string);
-        match (self.parse_iso_8601_date(&date_string), self.parse_iso_8601_time(&time_string)) {
-            (Some((a,b,c)), Some((d,e,f,g))) => ((a,b,c),(d,e,f,g)),
-            _ => panic!()
+        match (
+            self.parse_iso_8601_date(&date_string),
+            self.parse_iso_8601_time(&time_string),
+        ) {
+            (Some((a, b, c)), Some((d, e, f, g))) => ((a, b, c), (d, e, f, g)),
+            _ => panic!(),
         }
     }
 
     /// Parses ISO 8601 Date strings into LocalDate Object.
-    pub fn parse_iso_8601_date(&self, string: &str) -> Option<(u32, u32, u32)>
-    {
+    pub fn parse_iso_8601_date(&self, string: &str) -> Option<(u32, u32, u32)> {
         if let Some(caps) = self.ymd.captures(&string) {
-            Some((caps.get(1).unwrap().as_str().parse().unwrap(), // year
-                  caps.get(2).unwrap().as_str().parse().unwrap(), // month
-                  caps.get(3).unwrap().as_str().parse().unwrap(), // day
-                ))
+            Some((
+                caps.get(1).unwrap().as_str().parse().unwrap(), // year
+                caps.get(2).unwrap().as_str().parse().unwrap(), // month
+                caps.get(3).unwrap().as_str().parse().unwrap(), // day
+            ))
+        } else if let Some(caps) = self.week.captures(&string) {
+            Some((
+                caps.get(1).unwrap().as_str().parse().unwrap(), // year
+                caps.get(2).unwrap().as_str().parse().unwrap(), // week
+                caps.get(3).unwrap().as_str().parse().unwrap(), // weekday
+            ))
+        } else {
+            None
         }
-        else if let Some(caps) = self.week.captures(&string) {
-            Some((caps.get(1).unwrap().as_str().parse().unwrap(), // year
-                  caps.get(2).unwrap().as_str().parse().unwrap(), // week
-                  caps.get(3).unwrap().as_str().parse().unwrap()  // weekday
-                ))
-        }
-        else { None }
     }
 
-    pub fn parse_iso_8601_time(&self, string: &str) -> Option<(i8, i8, i8, i32)>
-    {
-        if let Some((hour, minute, second, millisecond, _zh, _zm, _z)) = self.parse_iso_8601_tuple(string) {
+    pub fn parse_iso_8601_time(&self, string: &str) -> Option<(i8, i8, i8, i32)> {
+        if let Some((hour, minute, second, millisecond, _zh, _zm, _z)) =
+            self.parse_iso_8601_tuple(string)
+        {
             return Some((hour, minute, second, millisecond));
         }
         None
     }
 
-    fn parse_iso_8601_tuple<'a>(&self, string: &'a str) -> Option<(i8,i8,i8,i32,i8,i8,&'a str)>
-    {
+    fn parse_iso_8601_tuple<'a>(
+        &self,
+        string: &'a str,
+    ) -> Option<(i8, i8, i8, i32, i8, i8, &'a str)> {
         if let Some(caps) = self.exp.captures(&string) {
-            let tup = (caps.get(1).map(|m|m.as_str()).unwrap_or("00").parse::<i8>().unwrap(), // HH
-                       caps.get(2).map(|m|m.as_str()).unwrap_or("00").parse::<i8>().unwrap(), // MM
-                       caps.get(3).map(|m|m.as_str()).unwrap_or("00").parse::<i8>().unwrap(), // SS
-                       caps.get(4).map(|m|m.as_str()).unwrap_or("000").parse::<i32>().unwrap(), // MS
-                       caps.get(6).map(|m|m.as_str()).unwrap_or("+00").trim_start_matches('+').parse::<i8>().unwrap(), // ZH
-                       caps.get(7).map(|m|m.as_str()).unwrap_or("00").parse::<i8>().unwrap(), // ZM
-                       caps.get(5).map(|m|m.as_str()).unwrap_or("_"), // "Z"
-                      );
+            let tup = (
+                caps.get(1)
+                    .map(|m| m.as_str())
+                    .unwrap_or("00")
+                    .parse::<i8>()
+                    .unwrap(), // HH
+                caps.get(2)
+                    .map(|m| m.as_str())
+                    .unwrap_or("00")
+                    .parse::<i8>()
+                    .unwrap(), // MM
+                caps.get(3)
+                    .map(|m| m.as_str())
+                    .unwrap_or("00")
+                    .parse::<i8>()
+                    .unwrap(), // SS
+                caps.get(4)
+                    .map(|m| m.as_str())
+                    .unwrap_or("000")
+                    .parse::<i32>()
+                    .unwrap(), // MS
+                caps.get(6)
+                    .map(|m| m.as_str())
+                    .unwrap_or("+00")
+                    .trim_start_matches('+')
+                    .parse::<i8>()
+                    .unwrap(), // ZH
+                caps.get(7)
+                    .map(|m| m.as_str())
+                    .unwrap_or("00")
+                    .parse::<i8>()
+                    .unwrap(), // ZM
+                caps.get(5).map(|m| m.as_str()).unwrap_or("_"), // "Z"
+            );
             if tup.3 > 0 && caps.get(4).map(|ms| ms.as_str().len()).unwrap_or(0) % 3 != 0 {
                 // println!("{}", tup.3);
-                return None
+                return None;
             }
             return Some(tup);
         }
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ extern crate test;
 extern crate chrono02;
 extern crate chrono03;
 extern crate chrono04;
-extern crate iso8601_v01;
-extern crate iso8601_v03;
 extern crate datetime;
 extern crate dtparse;
+extern crate iso8601_v01;
+extern crate iso8601_v03;
 extern crate regex;
 
 mod datetime_regex_pure;
@@ -24,9 +24,7 @@ mod chrono02_bench {
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
-            super::DATESTRING.parse::<DateTime<UTC>>()
-        });
+        b.iter(|| super::DATESTRING.parse::<DateTime<UTC>>());
     }
 }
 
@@ -38,9 +36,7 @@ mod chrono03_bench {
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
-            super::DATESTRING.parse::<DateTime<UTC>>()
-        });
+        b.iter(|| super::DATESTRING.parse::<DateTime<UTC>>());
     }
 }
 
@@ -52,21 +48,19 @@ mod chrono04_bench {
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
-            super::DATESTRING.parse::<DateTime<Utc>>()
-        });
+        b.iter(|| super::DATESTRING.parse::<DateTime<Utc>>());
     }
 }
 
 #[cfg(test)]
-mod regex_bench{
+mod regex_bench {
 
     use super::test::Bencher;
     use datetime_regex_pure::PureRegexParser;
 
     //#[bench]
     fn create_regex(b: &mut Bencher) {
-        b.iter(||{
+        b.iter(|| {
             PureRegexParser::new();
         });
     }
@@ -74,77 +68,74 @@ mod regex_bench{
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
         let parser = PureRegexParser::new();
-        b.iter(||{
+        b.iter(|| {
             parser.parse_iso_8601(super::DATESTRING);
         });
     }
 }
 
 #[cfg(test)]
-mod datetime_bench{
+mod datetime_bench {
 
     use super::test::Bencher;
-    use std::str::FromStr;
     use datetime::*;
+    use std::str::FromStr;
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
+        b.iter(|| {
             LocalDateTime::from_str(super::DATESTRING);
         });
     }
 }
 
 #[cfg(test)]
-mod iso8601_v01_bench{
+mod iso8601_v01_bench {
 
     use super::test::Bencher;
     use iso8601_v01::datetime;
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
+        b.iter(|| {
             datetime(super::DATESTRING);
         });
     }
 }
 
-
 #[cfg(test)]
-mod iso8601_v03_bench{
+mod iso8601_v03_bench {
 
     use super::test::Bencher;
     use iso8601_v03::datetime;
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
+        b.iter(|| {
             datetime(super::DATESTRING);
         });
     }
 }
 
-
 #[cfg(test)]
-mod dtparse_bench{
+mod dtparse_bench {
 
     use super::test::Bencher;
 
     #[bench]
     fn parse_iso8601(b: &mut Bencher) {
-        b.iter(||{
+        b.iter(|| {
             dtparse::parse(super::DATESTRING);
         });
     }
 }
 
-
 #[cfg(test)]
-mod completeness{
+mod completeness {
 
-    use std::str::FromStr;
     use datetime::LocalDateTime;
     use iso8601_v03::datetime as nomdatetime;
+    use std::str::FromStr;
 
     static ALL_FORMATS: &'static [&'static str] = &[
         "2015-10-24T16:30:48+00:00",
@@ -155,16 +146,12 @@ mod completeness{
         "2015-297T16:30:48Z",
     ];
 
-
-    static MINIMAL_FORMATS: &'static [&'static str] = &[
-        "2015-10-24T16:30:48+00:00",
-        "2015-10-24T16:30:48Z",
-    ];
-
+    static MINIMAL_FORMATS: &'static [&'static str] =
+        &["2015-10-24T16:30:48+00:00", "2015-10-24T16:30:48Z"];
 
     #[test]
     fn iso_week_date() {
-        for date in ALL_FORMATS.iter(){
+        for date in ALL_FORMATS.iter() {
             let parsed_by_chrono02 = date.parse::<chrono02::DateTime<chrono02::UTC>>();
             let parsed_by_datetime = LocalDateTime::from_str(date);
             let parsed_by_nom = nomdatetime(date);
@@ -182,44 +169,74 @@ mod completeness{
     #[test]
     fn minimal_chrono02() {
         use chrono02::{DateTime, UTC};
-        for date in MINIMAL_FORMATS.iter() { date.parse::<DateTime<UTC>>().unwrap(); }
+        for date in MINIMAL_FORMATS.iter() {
+            date.parse::<DateTime<UTC>>().unwrap();
+        }
     }
 
     #[test]
     fn minimal_chrono04() {
         use chrono04::{DateTime, Utc};
-        for date in MINIMAL_FORMATS.iter() { date.parse::<DateTime<Utc>>().unwrap(); }
+        for date in MINIMAL_FORMATS.iter() {
+            date.parse::<DateTime<Utc>>().unwrap();
+        }
     }
 
     #[test]
-    fn minimal_datetime() { for date in MINIMAL_FORMATS.iter() { LocalDateTime::from_str(date).unwrap(); } }
+    fn minimal_datetime() {
+        for date in MINIMAL_FORMATS.iter() {
+            LocalDateTime::from_str(date).unwrap();
+        }
+    }
 
     #[test]
-    fn minimal_nom() { for date in MINIMAL_FORMATS.iter() { nomdatetime(date).unwrap(); } }
+    fn minimal_nom() {
+        for date in MINIMAL_FORMATS.iter() {
+            nomdatetime(date).unwrap();
+        }
+    }
 
     #[test]
-    fn minimal_dtparse() { for date in MINIMAL_FORMATS.iter() { dtparse::parse(date).unwrap(); } }
-
+    fn minimal_dtparse() {
+        for date in MINIMAL_FORMATS.iter() {
+            dtparse::parse(date).unwrap();
+        }
+    }
 
     #[test]
     fn all_chrono02() {
         use chrono02::{DateTime, UTC};
-        for date in ALL_FORMATS.iter() { date.parse::<DateTime<UTC>>().unwrap(); }
+        for date in ALL_FORMATS.iter() {
+            date.parse::<DateTime<UTC>>().unwrap();
+        }
     }
 
     #[test]
     fn all_chrono04() {
         use chrono04::{DateTime, Utc};
-        for date in ALL_FORMATS.iter() { date.parse::<DateTime<Utc>>().unwrap(); }
+        for date in ALL_FORMATS.iter() {
+            date.parse::<DateTime<Utc>>().unwrap();
+        }
     }
 
     #[test]
-    fn all_datetime() { for date in ALL_FORMATS.iter() { LocalDateTime::from_str(date).unwrap(); } }
+    fn all_datetime() {
+        for date in ALL_FORMATS.iter() {
+            LocalDateTime::from_str(date).unwrap();
+        }
+    }
 
     #[test]
-    fn all_nom() { for date in ALL_FORMATS.iter() { nomdatetime(date).unwrap(); } }
+    fn all_nom() {
+        for date in ALL_FORMATS.iter() {
+            nomdatetime(date).unwrap();
+        }
+    }
 
     #[test]
-    fn all_dtparse() { for date in ALL_FORMATS.iter() { dtparse::parse(date).unwrap(); } }
-
+    fn all_dtparse() {
+        for date in ALL_FORMATS.iter() {
+            dtparse::parse(date).unwrap();
+        }
+    }
 }
